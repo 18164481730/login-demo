0b22eb1d08178d99f23df5aa27dc115fd9344211
Administrator <Administrator@192.168.56.1>
2018-01-21 19:50:34 +0800

    注销分页代码

--- src/main/java/com/tangzhihe/service/BookService.java
+++ src/main/java/com/tangzhihe/service/BookService.java
@@ -3,11 +3,8 @@ package com.tangzhihe.service;
 import java.util.List;
 
 import com.tangzhihe.domain.Book;
-import com.tangzhihe.util.Page;
 
 public interface BookService {
-	public Page<Book> queryBookListByPage(Page<Book> page,Book book);
-	
 	public List<Book> queryBookList(Book book);
 	
 	public int save(Book Book);
