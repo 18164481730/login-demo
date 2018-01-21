bdc37fba4392c501b7e04e3183f5542e871fe3b6
Administrator <Administrator@192.168.56.1>
2018-01-21 20:33:57 +0800

    删除黄色警告

--- src/main/java/com/tangzhihe/dao/UserDao.java
+++ src/main/java/com/tangzhihe/dao/UserDao.java
@@ -2,9 +2,9 @@ package com.tangzhihe.dao;
 
 import java.util.List;
 
-import com.tangzhihe.domain.Book;
-import com.tangzhihe.domain.User;
 import org.apache.ibatis.annotations.Param;
+
+import com.tangzhihe.domain.User;
 
 
 public interface UserDao {
