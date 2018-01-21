28f030b8d115daa027ada3a9212464437d5b6373
Administrator <Administrator@192.168.56.1>
2018-01-21 19:45:36 +0800

    事务注解

--- src/main/java/com/tangzhihe/service/impl/BookServiceImpl.java
+++ src/main/java/com/tangzhihe/service/impl/BookServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.tangzhihe.dao.BookDao;
 import com.tangzhihe.domain.Book;
@@ -12,6 +13,7 @@ import com.tangzhihe.util.Page;
 
 
 @Service
+@Transactional
 public class BookServiceImpl implements BookService {
 
     @Autowired
--- src/main/java/com/tangzhihe/service/impl/UserServiceImpl.java
+++ src/main/java/com/tangzhihe/service/impl/UserServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.tangzhihe.dao.UserDao;
 import com.tangzhihe.domain.User;
@@ -11,6 +12,7 @@ import com.tangzhihe.service.UserService;
 
 
 @Service
+@Transactional
 public class UserServiceImpl implements UserService{
 
 	@Autowired
